f9b34d88ab1fda2a788e276ce7fb3899567c197d
Wei Lun <weilun_95@hotmail.com>
2018-12-30 22:30:58 +0800

    added find needle haystack

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Typescript
 - [Find the odd int](typescript/src/index.ts#L8-18)
+- [A needle in the Haystack](typescript/src/index.ts#L21-L35)
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -18,3 +18,19 @@ export const findOdd = (xs: number[]): number => {
 };
 
 
+/**
+ * ### Can you find the needle in the haystack?
+ Write a function findNeedle() that takes an array full of junk but containing one "needle"
+ After your function finds the needle it should return a message (as a string) that says:
+ "found the needle at position " plus the index it found the needle, so:
+ ```typescript
+ findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']
+ ```
+ should return
+ "found the needle at position 5"
+ * @param haystack
+ */
+export function findNeedle(haystack: any[]): string {
+    return `found the needle at position ${haystack.indexOf('needle')}`
+}
+
