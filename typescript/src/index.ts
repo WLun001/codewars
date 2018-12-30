c735d70c0c1656bf36b2daf3ea72f40ddd2a72e9
Wei Lun <weilun_95@hotmail.com>
2018-12-30 22:07:32 +0800

    added find the odd int

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,4 +5,16 @@ function logItem(val: any) {
     document.getElementById("list").appendChild(node);
 }
 
+/**
+ * Given an array, find the int that appears an odd number of times.
+ * There will always be only one integer that appears an odd number of times.
+ * @param xs
+ */
+export const findOdd = (xs: number[]): number => {
+    const map = xs.reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map());
+    const sorted = [...map.entries()].sort();
+    const oddNumberArr = sorted.filter(x => x[1] % 2 !== 0);
+    return oddNumberArr.length !== 0 ? oddNumberArr[0][0] : 0;
+};
+
 
