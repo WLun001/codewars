6c1ac9603ccdb096f68f80ac2a1aad97d2fa10cd
Wei Lun <weilun_95@hotmail.com>
2018-12-30 22:02:43 +0800

    added typescript project

---
+++ typescript/src/index.ts
@@ -0,0 +1,8 @@
+function logItem(val: any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("list").appendChild(node);
+}
+
+
