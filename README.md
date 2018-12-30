52b7a3f928cc25def1284247de302035777501ab
Wei Lun <weilun_95@hotmail.com>
2018-12-30 22:38:09 +0800

    added tslint

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Typescript
 - [Find the odd int](typescript/src/index.ts#L8-L18)
-- [A needle in the Haystack](typescript/src/index.ts#L21-L35)
+- [A needle in the Haystack](typescript/src/index.ts#L20-L34)
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,6 @@
 function logItem(val: any) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(val);
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(val);
     node.appendChild(textnode);
     document.getElementById("list").appendChild(node);
 }
@@ -13,24 +13,22 @@ function logItem(val: any) {
 export const findOdd = (xs: number[]): number => {
     const map = xs.reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map());
     const sorted = [...map.entries()].sort();
-    const oddNumberArr = sorted.filter(x => x[1] % 2 !== 0);
+    const oddNumberArr = sorted.filter((x) => x[1] % 2 !== 0);
     return oddNumberArr.length !== 0 ? oddNumberArr[0][0] : 0;
 };
 
-
 /**
  * ### Can you find the needle in the haystack?
- Write a function findNeedle() that takes an array full of junk but containing one "needle"
- After your function finds the needle it should return a message (as a string) that says:
- "found the needle at position " plus the index it found the needle, so:
- ```typescript
- findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']
- ```
- should return
- "found the needle at position 5"
+ * Write a function findNeedle() that takes an array full of junk but containing one "needle"
+ * After your function finds the needle it should return a message (as a string) that says:
+ * "found the needle at position " plus the index it found the needle, so:
+ * ```typescript
+ * findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']
+ * ```
+ * should return
+ * "found the needle at position 5"
  * @param haystack
  */
 export function findNeedle(haystack: any[]): string {
-    return `found the needle at position ${haystack.indexOf('needle')}`
+    return `found the needle at position ${haystack.indexOf("needle")}`;
 }
-
